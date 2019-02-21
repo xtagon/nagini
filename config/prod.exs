6d12a147a86ee9397cebea6a09893392185c795c
Justin Workman <xtagon@gmail.com>
2019-02-20 19:44:16 -0800

    Enable debug logging in production.

--- config/prod.exs
+++ config/prod.exs
@@ -14,8 +14,8 @@ config :nagini, NaginiWeb.Endpoint,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
-# Do not print debug messages in production
-config :logger, level: :info
+# Print debug messages in production
+config :logger, level: :debug
 
 # ## SSL Support
 #
