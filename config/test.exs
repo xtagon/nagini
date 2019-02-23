b757adde98284c513f748fafe3bc78e76b3f16d2
Justin Workman <xtagon@gmail.com>
2019-02-23 13:37:37 -0800

    Print debug logs in test.

--- config/test.exs
+++ config/test.exs
@@ -6,5 +6,5 @@ config :nagini, NaginiWeb.Endpoint,
   http: [port: 4002],
   server: false
 
-# Print only warnings and errors during test
-config :logger, level: :warn
+# Print only everything during test (it's useful for now)
+config :logger, level: :debug
